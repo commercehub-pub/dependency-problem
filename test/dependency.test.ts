f5affc10e5939069fa801b44bbe667e229b6f518
Rich Mischler <rich.mischler@blackboard.com>
2022-06-09 20:40:22 -0400

    feat: add tests for both a 'no conflicts' response and a response containing conflicts

--- test/dependency.test.ts
+++ test/dependency.test.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { Server } from 'http';
 import { createApp } from '../src/app';
 
-describe('/package/:name/:version endpoint', () => {
+describe('/package/:name/:versionOne/:versionTwo endpoint', () => {
   let server: Server;
   let port: number;
 
   beforeAll(async () => {
-    port = 3000
+    port = 3000;
     server = createApp().listen(port);
   });
 
@@ -15,15 +15,27 @@ describe('/package/:name/:version endpoint', () => {
     server.close(done);
   });
 
-  it('responds', async () => {
+  it('test no conflicting dependency versions', async () => {
     const packageName = 'react';
-    const packageVersion = '16.13.0';
+    const packageVersionOne = '16.13.0';
+    const packageVersionTwo = '16.13.1';
 
     const res: any = (await axios(
-      `http://localhost:${port}/dependency/${packageName}/${packageVersion}`,
+      `http://localhost:${port}/dependency/${packageName}/${packageVersionOne}/${packageVersionTwo}`,
     )).data;
 
-    expect(res.name).toEqual(packageName);
-    expect(res.version).toEqual(packageVersion);
+    expect(res.differences).toEqual('No conflicts');
+  });
+
+  it('test conflicting dependency versions', async () => {
+    const packageName = 'react';
+    const packageVersionOne = '15.0.1';
+    const packageVersionTwo = '16.13.1';
+
+    const res: any = (await axios(
+      `http://localhost:${port}/dependency/${packageName}/${packageVersionOne}/${packageVersionTwo}`,
+    )).data;
+
+    expect(res.differences.length).toBeGreaterThan(0);
   });
 });
